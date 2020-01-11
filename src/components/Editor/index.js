c8e8bf205b8314bd7407d6ccf6d60ed0a593d33c
Aristoteles <aristotelesbr2014@gmail.com>
2020-01-11 18:17:43 -0300

    Start project and added small header

---
+++ src/components/Editor/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function Editor() {
+  return (
+    <>
+      <h1>Editor</h1>
+    </>
+  );
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// import { Container } from './styles';
+
+export default function Header() {
+  return (
+    <>
+      <h1>Header Maroto</h1>
+    </>
+  );
+}
